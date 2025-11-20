039d861907887da3647d7ee7a3c8dd4fbd0fe043
DozingDozerZZZZ <763664@student.dusd.net>
2025-11-20 15:28:46 -0800

    Added tilemap collsion and fixed movemennt positonj
    move tiles into tiles folder

---
+++ P7JunScriptingProject/Assets/Scripts/Looping Scripts/DoWhileLoops.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoops : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //bool shouldContiune = false;
+
+      //  do
+        {
+           // print("Hello world.");
+
+          //  while; (shouldContiune == true);
+        } 
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
